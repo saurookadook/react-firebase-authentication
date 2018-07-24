87d899af6c2f0bd019c38ff1b05620dd058ca11f
saurookadook <maskiella@gmail.com>
2018-07-23 21:28:29 -0400

    Build out files in src/firebase directory

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const config = {
+// const prodConfig = { OPTIONAL SECOND STEP - create second project for production so that data from deployed app isn't confused with data in dev app
+//     apiKey: REACT_APP_API_KEY,
+//     authDomain: REACT_APP_AUTH_DOMAIN,
+//     databaseURL: REACT_APP_DATABASE_URL,
+//     projectId: REACT_APP_PROJECT_ID,
+//     storageBucket: REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
+// };
+
+const config = { // would make this `devConfig` if taking the optional step above
     apiKey: REACT_APP_API_KEY,
     authDomain: REACT_APP_AUTH_DOMAIN,
     databaseURL: REACT_APP_DATABASE_URL,
@@ -9,6 +18,10 @@ const config = {
     storageBucket: REACT_APP_STORAGE_BUCKET,
     messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
 };
+
+// const config = process.env.NODE_ENV === 'production'  // additional setup for optional step
+//   ? prodConfig
+//   : devConfig;
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
