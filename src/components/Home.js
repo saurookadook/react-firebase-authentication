2ed534e3563fbb94f9a346c65122687b825513e5
saurookadook <maskiella@gmail.com>
2018-08-02 13:49:29 -0400

    Enable authorization protection for Home and Account components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import withAuthorization from './withAuthorization';
+
 const HomePage = () =>
   <div>
     <h1>Home Page</h1>
+    <p>{`The Home Page is accesible by every signed-in User.`}</p>
   </div>
 
-export default HomePage;
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(HomePage);
